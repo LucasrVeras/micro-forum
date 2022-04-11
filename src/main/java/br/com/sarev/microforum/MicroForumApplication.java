88af32374553d6ebebc9f375786d403ed386f030
Lucas Veras <lucas.veras01@gmail.com>
2022-04-11 04:44:54 -0300

    Ordenação e paginação

--- src/main/java/br/com/sarev/microforum/MicroForumApplication.java
+++ src/main/java/br/com/sarev/microforum/MicroForumApplication.java
@@ -4,8 +4,15 @@ package br.com.sarev.microforum;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+ 
+/* 
+ * Suporte para pegar da requisição os paramentros da url os campos de 
+ * paginação  e ordenação passando tudo para o SpringData 
+ */ 
+@EnableSpringDataWebSupport
 public class MicroForumApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
