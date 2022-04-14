b74e1fe903b87980f30a4c8d6a700c483d1a5655
Lucas Veras <lucas.veras01@gmail.com>
2022-04-14 14:00:54 -0300

    Testes JUnit

---
+++ src/test/java/br/com/sarev/microforum/MicroForumApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.sarev.microforum;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class MicroForumApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		Assert.assertTrue(true);
+	}
+}
---
+++ src/test/java/br/com/sarev/microforum/repositories/CursoRepositoryTest.java
@@ -0,0 +1,27 @@
+package br.com.sarev.microforum.repositories;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import br.com.sarev.microforum.modelo.Curso;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+class CursoRepositoryTest {
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+	
+	@Test
+    public void carregarCursoPeloSeuNome() {
+		
+		String nomeCurso = "JavaScript";
+		Curso curso = cursoRepository.findByNome(nomeCurso);
+		Assert.assertNotNull(curso);
+		Assert.assertEquals("JavaScript" ,curso.getNome());
+	}
+}
